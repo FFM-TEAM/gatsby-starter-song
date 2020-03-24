e2563314cffa7fb9b12a1a77b1c50f63e6b7fa6a
s-ong-c <somony9292@gmail.com>
2020-03-24 18:49:22 +0900

    html build

--- gatsby-node.js
+++ gatsby-node.js
@@ -63,3 +63,18 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     });
   }
 };
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === 'build-html') {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /bad-module/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    });
+  }
+};
--- src/lib/heading.ts
+++ src/lib/heading.ts
@@ -1,38 +1,9 @@
-import { escapeForUrl } from './utils';
-
-/**
- * Set unique id for each headings (only for h1, h2, h3)
- * @param html
- */
-export function setHeadingId(html: string) {
-  const div = window.document.createElement('div');
-  div.innerHTML = html;
-
-  const h1 = div.querySelectorAll('h1');
-  const h2 = div.querySelectorAll('h2');
-  const h3 = div.querySelectorAll('h3');
-
-  const idList: string[] = [];
-
-  const setId = (element: HTMLHeadingElement) => {
-    const id = escapeForUrl(element.innerText);
-    const exists = idList.filter(existingId => existingId.indexOf(id) !== -1);
-    const uniqueId = `${id}${exists.length === 0 ? '' : `-${exists.length}`}`;
-    element.id = uniqueId;
-    idList.push(uniqueId);
-  };
-
-  [h1, h2, h3].forEach(elements => elements.forEach(setId));
-
-  return div.innerHTML;
-}
-
 /**
  * Parses heading for building TOC
  * @param html
  */
 export function parseHeadings(html: string) {
-  const div = window.document.createElement('div');
+  const div = document.createElement('div');
   div.innerHTML = html;
 
   const elements = Array.from(div.children);
